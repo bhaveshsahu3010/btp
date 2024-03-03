80dd482b49c0354b10414c009b809121d1cfacde
Bhavesh Sahu <bhavesh.sahu3010@gmail.com>
2024-03-03 20:00:16 +0530

    3March

--- src/components/Dashboard1.jsx
+++ src/components/Dashboard1.jsx
@@ -2,15 +2,25 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 function Dashboard1() {
-  return (
-    <div className='Db'>
-      <h1>Hi User</h1>
-      <div className='upload_project'>
-        <Link to="/uploadProject"></Link>
-      </div>
-      <div className='see_student_list'></div>
-    </div>
-  )
+    return (
+        <div className='Db'>
+            <h1>Hi User</h1>
+            <div className='dash1'>
+
+                <Link to="/uploadProject" className='text1'>
+                    <div className='upload_project'>
+                        Upload A new project
+                    </div>
+                </Link>
+                <Link to="/studentList" className='text2'>
+                    <div className='see_student_list'>
+                        Student List
+
+                    </div>
+                </Link>
+            </div>
+        </div>
+    )
 }
 
 export default Dashboard1
